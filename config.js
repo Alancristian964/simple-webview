161f447a7bad8d684a1d3f4cea304fe018ffd8c4
Alan Cristian <alancristian964@gmail.com>
2020-05-23 09:45:59 -0300

    Simple-Webview

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  urlLocalhost: "http://localhost:3000",
+  urlYoutube: "http://youtube.com.br",
+  urlGoogle: "http://google.com.br",
+  urlTranslateEnPt: "https://translate.google.com.br/?hl=pt-BR#view=home&op=translate&sl=en&tl=pt",
+  urlTranslatePtEn: "https://translate.google.com.br/?hl=pt-BR#view=home&op=translate&sl=pt&tl=en",
+}
